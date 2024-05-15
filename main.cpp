20a2c69c89e46f493f33ebec9645658288fe9ae8
Rigrey <ozhogin.m@gmail.com>
2024-05-15 21:55:01 +0300

    its going to be better

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "Proxy.cpp"
-
+#include <fstream>
 int main()
 {
     Proxy *proxy = new Proxy();
     proxy->request();
 
     delete proxy;
+
+    std::ofstream a("log.txt");
+    a << "thingy thing";
     return 0;
 }
